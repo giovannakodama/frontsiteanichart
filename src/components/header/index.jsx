4804c22430a3e515274690ebcc07a6049c2c2618
TiagoHasuda <tiago.ghasuda@gmail.com>
2021-12-16 21:08:44 -0300

    fix: verificação login

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -51,6 +51,14 @@ export const Header = () => {
 
     const onLogin = () => {
         if (loading) return
+        if (!email || !password) {
+            setError('Preencha todos os campos!')
+            return
+        }
+        if (email.match(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/).length == 0) {
+            setError('Email inválido!')
+            return
+        }
         setError('')
         setLoading(true)
         const xHttp = new XMLHttpRequest()
